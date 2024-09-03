3b6dd75543aebed5bb21c6c10d86891dc21728ff
SalTor <sal@saltor.nyc>
2024-09-02 20:49:03 -0400

    add problem set for filling prescriptions

---
+++ src/filling-prescriptions/index.test.ts
@@ -0,0 +1,21 @@
+describe("Add inventory for N X", () => {
+  test("Filling for N X works", () => {
+    //
+  });
+
+  test("Filling for N+1 X does not work", () => {
+    //
+  });
+});
+
+describe("Define generic Y for X", () => {
+  describe("Has inventory for 100 X and 200 Y", () => {
+    test("Filling for 150 X and generic is acceptable", () => {
+      //
+    });
+
+    test("Filling for 150 X and generic is not acceptable", () => {
+      //
+    });
+  });
+});
